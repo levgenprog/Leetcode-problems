60dedc1e3bc6f006bfe901c6397b51300b7a699f
levgen <levgenprog@ya.ru>
2023-04-06 21:35:38 +0600

    partial solutuon

--- yandex/shablonizator/script.js
+++ yandex/shablonizator/script.js
@@ -1,8 +1,10 @@
 function solution(entryPoint) {
     const innerElements = entryPoint.querySelectorAll('*');
-    //array of objects
+
     let cpArr = [];
     let remArr = [];
+    let remChildrenArr = [];
+    let switchArr = [];
 
     for (const el of innerElements) {
         let command = el.getAttribute('x-make');
@@ -23,28 +25,78 @@ function solution(entryPoint) {
                     });
                     console.log(remArr);
                     break;
+                case 'removeChildren':
+                    remChildrenArr.push({
+                        elem: el,
+                        n: parseInt(cmd[1])
+                    });
+                    break;
+                case 'switch':
+                    switchArr.push({
+                        elem: el,
+                        n: parseInt(cmd[1])
+                    });
+                    break;
             }
         }
     }
 
+
+
+    // for (let i = 0; i < 3; i++) {
     for (const obj of cpArr) {
-        for (let i = 0; i < obj.n; i++) {
-            obj.elem.removeAttribute('x-make');
-            let copy = obj.elem.cloneNode(true);
-            obj.elem.after(copy);
+        if (obj.elem.parentNode !== null) {
+            if (!obj.elem.parentNode.hasAttribute('x-make')) {
+                obj.elem.removeAttribute('x-make');
+                for (let i = 0; i < obj.n; i++) {
+                    let copy = obj.elem.cloneNode(true);
+                    obj.elem.after(copy);
+                }
+            }
         }
     }
+
 
     for (const obj of remArr) {
-        obj.elem.removeAttribute('x-make');
-        for (let i = 0; i < obj.n; i++) {
-            let nextEl = obj.elem.nextElementSibling;
-            console.log(nextEl);
-            if (nextEl) {
-                nextEl.remove();
+        if (obj.elem.parentNode !== null) {
+            if (!obj.elem.parentNode.hasAttribute('x-make')) {
+                obj.elem.removeAttribute('x-make');
+                for (let i = 0; i < obj.n; i++) {
+                    let nextEl = obj.elem.nextElementSibling;
+                    console.log(nextEl);
+                    if (nextEl) {
+                        nextEl.remove();
+                    }
+                }
             }
         }
     }
+
+    for (const obj of remChildrenArr) {
+        if (obj.elem.parentNode !== null) {
+            if (!obj.elem.parentNode.hasAttribute('x-make')) {
+                obj.elem.removeAttribute('x-make');
+                const childrenArr = obj.elem.querySelectorAll('*');
+                for (let i = 0; i < obj.n; i++) {
+                    obj.elem.removeChild(childrenArr[i]);
+                }
+            }
+        }
+    }
+
+
+    for (const obj of switchArr) {
+        if (obj.elem.parentNode !== null) {
+            if (!obj.elem.parentNode.hasAttribute('x-make')) {
+                obj.elem.removeAttribute('x-make');
+                obj.elem.parentNode.insertBefore(obj.elem, obj.elem.parentNode.childNodes[obj.n]);
+            }
+        }
+    }
+    // }
+
+
+
 
     // console.log(innerElements);
 }
